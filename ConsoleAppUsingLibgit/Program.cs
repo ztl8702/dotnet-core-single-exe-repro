46826afabfe3db3c70b80bd586047e2ebd2a891d
Radium Zheng <dtienloi@google.com>
2019-10-15 04:44:58 +1100

    create sample app

---
+++ ConsoleAppUsingLibgit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace ConsoleAppUsingLibgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This app uses LibGit2Sharp");
+            try
+            {
+                using (var repo = new Repository("./"))
+                {
+                    var latestCommit = repo.Commits.First();
+                    Console.WriteLine($"Latest commit: {latestCommit.Message}");
+                }
+            }
+            catch (LibGit2Sharp.RepositoryNotFoundException)
+            {
+                Console.WriteLine("Oops, this is not a git repo.");
+            }
+
+        }
+    }
+}
