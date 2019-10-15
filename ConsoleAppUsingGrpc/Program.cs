7f81c8d3f08d907855345cbe1517d8a0874ae8e7
Radium Zheng <dtienloi@google.com>
2019-10-15 22:24:22 +1100

    grpc

---
+++ ConsoleAppUsingGrpc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Helloworld;
+using Grpc.Core;
+
+namespace ConsoleAppUsingGrpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new HelloRequest { Name = "world" };
+            Console.WriteLine($"Hello {x.Name} using Grpc!");
+
+            try
+            {
+                // This will probably fail. But it doesn't matter.
+                // We just need to keep a reference to Grpc.
+                Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+                var client = new Greeter.GreeterClient(channel);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
